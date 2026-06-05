50d88bc3f67f6fec56ef43d1187be9a16201e45a
Kevin Caccamo <kevin@ciinet.org>
2026-06-04 22:52:55 -0400

    Initial commit
    A small game window with SDL

---
+++ README.md
@@ -0,0 +1,6 @@
+Shoot It!
+=========
+
+My entry for [LCOLONQ MICROJAM 2K26-A-PALOOZA](https://api.colonq.computer/jam/2026)
+
+Licensed under the MIT License
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+/*
+ * Shoot It (c) Talon1024 2026-present
+ *
+ * MIT License
+ */
+
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    SDL_SetAppMetadata("Shoot It!", "0.0", "io.github.Talon1024.ShootIt");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("Shoot It!", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program.
+ SAFETY: The renderer is most likely initialized.
+ */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    if (!SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255)) {
+        SDL_Log("Could not set clear colour");
+    }
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
+}
